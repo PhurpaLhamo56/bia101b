aa58ef7801de5d6184e84913e7ec8132315e2915
phurpa lhamo <03230232.gcbs@rub.edu.bt>
2024-03-01 13:01:08 -0800

    conditionals and indentention

---
+++ conditionals.py
@@ -0,0 +1,27 @@
+#. objectives: calculator.application.made.using.
+#.variables.and.if. statements
+
+# steps
+# 1. get input from user
+# 2. do calculation based on user input
+# 2.1 check what string did user typed
+# 2.2 
+# 3. give output to the user
+
+print('* for multiplication')
+print('+ for addition')
+print('- for subtration')
+print('/ for division')
+
+whatUserTyped = input()
+
+print('User typed:', whatUserTyped)
+print('user input-type', type(whatUserTyped))
+
+if whatUserTyped == "+":
+    print('Doing Additon')
+    print('doing more addtion')
+
+    if whatUsertyped == "-":
+        print('Doing substration')
+        print('doing more substration')
---
+++ datatype.py
@@ -0,0 +1,7 @@
+variable1 = 18
+name = 'Dechen' # string
+name2 = "Dechen2"
+score = 10.1 # float/decimal
+didhewin = True # boolean
+#...
+print(variable1)

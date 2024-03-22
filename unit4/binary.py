7fd66eaa1df8fc51d61d0ed64b38c707f278911b
phurpa lhamo <03230232.gcbs@rub.edu.bt>
2024-03-21 23:41:41 -0700

    rpc1

---
+++ unit4/binary.py
@@ -0,0 +1,29 @@
+# binary search
+
+# input array
+arr = [3,4,5,6,7,8.9]
+target = 7
+
+low = 0
+high = len(arr) - 1
+
+# loop
+result = False
+while low <= high:
+    #divide
+    mid = (low + high) //2 # this is middle index(not the value of the arr)
+
+    # compare the middle with target
+    if arr[mid] == target:
+        result = True
+        break
+    
+    #compare and discard the half
+    if target > arr[mid]:
+        low = mid + 1
+    else:
+        high = mid - 1
+if result == True:
+    print('Found')
+else:
+    print("Not found")

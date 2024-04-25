ef9b028ebfd3b94c64d696755781eb1e4a45a969
phurpa lhamo <03230232.gcbs@rub.edu.bt>
2024-04-25 02:12:32 -0700

    oop

--- oop exe.py
+++ oop exe.py
@@ -1,36 +1,40 @@
 class Dog:
-    def __init__(name, self, breed, age, color):
+    def __init__(self, name, breed, age, color):
         self.name = name
         self.breed = breed
         self.age = age
         self.color = color
 
-    
-    def introduction(self): 
+    def introduce(self):
         print("Hi")
-        print("I am a " + self.breed + " dog.") 
-        print("I am a " + str(self.age) + " years old")
-        print(" I am a "+ self.color + " in color")      
+        print("I am a " + self.breed + " dog.")
+        print("I am " + str(self.age) + " years old")
+        print("I am " + self.color + " in color")
+
+    def say_age(self):
+        words_to_say = "I am " + str(self.age) + " years old"
+        print(words_to_say)
+
     def bark(self):
-        print("Woof! Woof!")
-
-    def sleep(self):
-        print("Zzzz...")
-
-    def eat(self):
-        print("Nom nom nom!")    
-
-dog = Dog('phurpa', 'bhutanese', 20, 'black')
-petdog = Dog('lhamo', 'pug', 10, 'white')
-
-dog.say_age()
-petdog.say_age()
-dog.introduction()
-print(introdution)
-
-dog.eat()
-dog.sleep()
-dog.bark()   
+        print('Woof woof')
     
+    def sleep(self):
+        print('Zzzzzz')
+    
+    def eat(self):
+        print('i am eating')
+    
+    def run(self, speed):
+        print('I am', self.name)
+        print('Okay, i will run in', str(speed), 'km/hr')
+        print()
 
+dog = Dog('dechen', 'bhutanese', 20, 'black')
+petdog = Dog('dorji', 'pug', 10, 'white')
+my_friend_dog = Dog('tashi', 'german shephard', 15, 'brown')
 
+how_fast_should_dog_run = 1000
+dog.run(10)
+dog.run(100)
+dog.run(how_fast_should_dog_run)
+petdog.run(20)
